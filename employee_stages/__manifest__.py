0dd20fef8de50d310a5b5266699573cd2e2ee230
Nirav Jani <nirav@niodigital.co>
2022-02-23 15:33:10 +0530

    change new update.

--- employee_stages/__manifest__.py
+++ employee_stages/__manifest__.py
@@ -4,7 +4,8 @@
     'name': 'Employee Stages',
     'version': '15.0.1.0.0',
     'summary': """Manages Employee Stages""",
-    'description': """This module is used to tracking employee's different stages.""",
+    'description': """This module is used to
+    tracking employee's different stages.""",
     'category': "Generic Modules/Human Resources",
     'author': 'ABC',
     'company': 'abc',
