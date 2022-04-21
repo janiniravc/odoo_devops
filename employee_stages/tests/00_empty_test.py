f9d3facc574ed1325087a4cd2258f153b8a1e079
Nirav Jani <nirav@niodigital.co>
2022-04-21 15:31:41 +0530

    added test case

--- employee_stages/tests/00_empty_test.py
+++ employee_stages/tests/00_empty_test.py
@@ -1,3 +1,6 @@
+from odoo.tests import Form
+from odoo.addons.hr.tests.common import TestHrCommon
+
 def test_empty():
     """
     PyTest tests are callables whose names start with "test"
@@ -5,7 +8,16 @@ def test_empty():
 
     It looks for them in modules whose name starts with "test_" or ends with "_test"
     (by default)
+    
     """
+	_tz = 'Pacific/Apia'
+	self.res_users_hr_officer.company_id.resource_calendar_id.tz = _tz
+	Employee = self.env['hr.employee'].with_user(self.res_users_hr_officer)
+	employee_form = Form(Employee)
+	employee_form.name = 'Raoul Grosbedon'
+	employee_form.work_email = 'raoul@example.com'
+	employee = employee_form.save()
+	self.assertEqual(employee.tz, _tz)
     pass
 
 
